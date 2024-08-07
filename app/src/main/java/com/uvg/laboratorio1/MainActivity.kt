9d2ee26c2f126ccf2ec335f5461eee7eef4c2aca
Ihan-Marroquin <ihanmarroquin12@gmail.com>
2024-08-06 22:51:12 -0600

    Add more configurations

--- app/src/main/java/com/uvg/laboratorio1/MainActivity.kt
+++ app/src/main/java/com/uvg/laboratorio1/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.uvg.laboratorio1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
 import com.uvg.laboratorio1.ui.theme.Laboratorio1Theme
 
@@ -20,8 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Laboratorio1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainContent(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +38,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = "Laboratorio 1",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Text(
+            text = "Ihan Marroquin",
+            fontSize = 18.sp,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Text(
+            text = "23108",
+            fontSize = 18.sp,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        MyButton()
+    }
+}
+
+@Composable
+fun MyButton() {
+    Button(
+        onClick = {
+            Log.d("MainActivity", "mi primer boton, emocion total c:")
+        },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Blue,
+            contentColor = Color.White
+        ),
+        modifier = Modifier.padding(vertical = 8.dp)
+    ) {
+        Text(text = "Presionar", fontSize = 16.sp)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Laboratorio1Theme {
-        Greeting("Android")
+        MainContent()
     }
 }
